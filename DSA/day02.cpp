49ff93b2ba7d482f33cda83ded3923ead7963048
TusharPatwal <patwaltushar001@gmail.com>
2022-12-17 10:11:18 +0530

    Update

--- DSA/day02.cpp
+++ DSA/day02.cpp
@@ -46,22 +46,27 @@ int main()
     */
 
     // prime number
-    int n, count = 0;
+    int i, n;
+    bool is_prime = true;
+
+    cout << "Enter a number: ";
     cin >> n;
-    for (int i = 2; i < n; i++)
-    {
-        if (n % i == 0)
-        {
-            count = 1;
-            break;
+
+    // 0 and 1 are not prime numbers
+    if (n == 0 || n == 1) {
+        is_prime = false;
+    }
+
+    // loop to check if n is prime
+    for (i = 2; i <= n/2; ++i) {
+        if (n % i == 0) {
+        is_prime = false;
+        break;
         }
     }
-    if (count != 0)
-    {
-        cout << "Not Prime Number";
-    }
+
+    if (is_prime)
+        cout << n << " is a prime number";
     else
-    {
-        cout << "Prime number";
-    }
+        cout << n << " is not a prime number";
 }
