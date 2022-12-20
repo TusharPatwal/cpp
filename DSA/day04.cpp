6e4de407851f3b5d4a1ecdc3655775098a14b89f
TusharPatwal <patwaltushar001@gmail.com>
2022-12-20 18:21:49 +0530

    update

--- DSA/day04.cpp
+++ DSA/day04.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main(){
 
     // decimal to binary
+    /*
     int n;
     cin>>n;
     int i = 0, ans = 0;
@@ -13,6 +14,44 @@ int main(){
 
         ans = (digit * pow(10,i))+ ans;
 
+        n = n>>1;
+        i++;
+    }
+    cout<<ans<<endl;
+    */
+
+
+    // binary to decimal
+    /*
+    int n;
+    cin>>n;
+    int i = 0, ans = 0;
+    while(n!=0){
+        int digit = n%10;
+
+        if(digit==1){
+            ans = ans + pow(2,i);
+        }
+
+        n /= 10;
+        i++;
+    }
+    cout<<ans<<endl;
+    */
+
+
+    // negative decimal to binary
+
+    long long int n;
+    cin>>n;
+    unsigned long long int i = 0, ans = 0;
+    if(n<0){
+        n = pow(2,16)+n;
+    }
+    // cout<<n<<endl;
+    while(n){
+        int lastbit = n&1;
+        ans = (pow(10,i)+lastbit)+ans;
         n = n>>1;
         i++;
     }
