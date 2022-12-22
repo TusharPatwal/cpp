219bd2f3eea24f030a45928dedc5fd95ae65687e
TusharPatwal <patwaltushar001@gmail.com>
2022-12-22 16:01:25 +0530

    update

--- DSA/day06.cpp
+++ DSA/day06.cpp
@@ -1,6 +1,36 @@
 #include<iostream>
 using namespace std;
 
+
+//0 -> not a prime number
+//1 -> prime number
+int isPrime(int n){
+    // bool prime = false;
+    if(n == 0 || n == 1){
+        return 0;
+    }
+    else{
+        for(int i=2;i<n;i++){
+            if(n%i==0){
+                return 0;
+            }
+        }
+        return 1;
+    }
+}
+
+int main(){
+    int n; cin>>n;
+    int ans = isPrime(n);
+    if (ans == 0){
+        cout<<n<<" is not a prime number"<<endl;
+    }
+    else{
+        cout<<n<<" is a prime number"<<endl;
+    }
+}
+
+/*
 int factorial(int n){
     int fact = 1;
     for(int i = 1; i<=n; i++){
@@ -19,6 +49,9 @@ int main(){
     // int n; cin>>n;
     cout<<nCr(13,10);
 }
+*/
+
+
 
 /*
 // Odd or Even
