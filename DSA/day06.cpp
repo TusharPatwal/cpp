36c3c7bd4b74173eeb4d7afbec0f882e169c78f3
TusharPatwal <patwaltushar001@gmail.com>
2022-12-22 19:19:26 +0530

    update

--- DSA/day06.cpp
+++ DSA/day06.cpp
@@ -1,9 +1,73 @@
 #include<iostream>
+#include<math.h>
 using namespace std;
+
+int a_b(int n){
+    int i = 0, ans = 0;
+    while(n!=0){
+        int digit = n&1;
+
+        ans = (digit * pow(10,i))+ ans;
+
+        n = n>>1;
+        i++;
+    }
+    return ans;
+}
+
+// void fibonnaci(int n){
+//     int a1 = 0;
+//     int a2 = 1;
+//     cout<<a1<<" "<<a2<<" ";
+//     for(int i=1; i<=n; i++){
+//         int a3 = a1+a2;
+//         a1 = a2;
+//         a2 = a3;
+//         cout<<a3<<" ";
+//     }
+// }
+int len(int n){
+    int len = 1;
+    if (n != 0) {
+        for (len = 0; n > 0; len++) {
+            n = n / 10;
+    }
+    }
+    return len;
+}
+int main(){
+    // int n;
+    // cin>>n;
+    // fibonnaci(n);
+    int a, b;
+    cin>>a>>b;
+    a = a_b(a);
+    b = a_b(b);
+    cout<<a<<endl<<b<<endl;
+    
+}
+
+
+
+/*
+int ap(int n){
+    int ans = (n*3)+7;
+    return ans;
+}
+
+int main(){
+    int n;cin>>n;
+    cout<<ap(n);
+}
+*/
 
 
 //0 -> not a prime number
 //1 -> prime number
+
+
+/*
+// function for prime function
 int isPrime(int n){
     // bool prime = false;
     if(n == 0 || n == 1){
@@ -29,6 +93,8 @@ int main(){
         cout<<n<<" is a prime number"<<endl;
     }
 }
+*/
+
 
 /*
 int factorial(int n){
