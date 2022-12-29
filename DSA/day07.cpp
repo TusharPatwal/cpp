a93a156e59748c774c3d5d855a279773bc399114
TusharPatwal <patwaltushar001@gmail.com>
2022-12-29 13:18:46 +0530

    update

--- DSA/day07.cpp
+++ DSA/day07.cpp
@@ -38,12 +38,12 @@ int main(){
 
 
 
-    // int arr[10];
-    // int a = 8;
-    // for(int i = 0; i<10; i++){
-    //     arr[i] = a;
-    // }
-    // for(int i = 0; i<10; i++){
-    //     cout<<arr[i]<<" ";
-    // }
+    int arr[10];
+    int a = 8;
+    for(int i = 0; i<10; i++){
+        arr[i] = a;
+    }
+    for(int i = 0; i<10; i++){
+        cout<<arr[i]<<" ";
+    }
 }
