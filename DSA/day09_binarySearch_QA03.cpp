830f7219906708a3a75db2f2a6feb2d20f3d60e6
TusharPatwal <patwaltushar001@gmail.com>
2023-01-01 18:59:56 +0530

    find the maximum in given array

---
+++ DSA/day09_binarySearch_QA03.cpp
@@ -0,0 +1,10 @@
+// find the maximum in given array
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+
+int main(){
+    vector<int> v = {1,2,3,4,5,6,7,8};
+    // int maxi = max(v.begin(), v.end());
+}
