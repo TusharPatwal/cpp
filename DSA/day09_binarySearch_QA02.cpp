cc50e18a066226c4b30c082e780d16520b3f9110
TusharPatwal <patwaltushar001@gmail.com>
2023-01-01 20:20:51 +0530

    find total number of occurence of an element in array

--- DSA/day09_binarySearch_QA02.cpp
+++ DSA/day09_binarySearch_QA02.cpp
@@ -1,54 +1,49 @@
 // find total number of occurence of an element in array
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int firstOcc(int arr[], int n, int key){
-    int s = 0;
-    int e = n-1;
-    int mid = s+(e-s)/2;
-    int ans = -1;
-    while(s<=e){
+int firstOcc(int arr[], int n, int key) {
+  int s = 0;
+  int e = n - 1;
+  int mid = s + (e - s) / 2;
+  int ans = -1;
+  while (s <= e) {
 
-        if(arr[mid]==key){
-            ans = mid;
-            e = mid - 1; 
-        }
-        else if(key>arr[mid]){
-            s = mid + 1;
-        }
-        else if(key<arr[mid]){
-            e = mid - 1;
-        }
-        mid = s+(e-s)/2;
+    if (arr[mid] == key) {
+      ans = mid;
+      e = mid - 1;
+    } else if (key > arr[mid]) {
+      s = mid + 1;
+    } else if (key < arr[mid]) {
+      e = mid - 1;
     }
-    return ans;
+    mid = s + (e - s) / 2;
+  }
+  return ans;
 }
-int lastOcc(int arr[], int n, int key){
-    int s = 0;
-    int e = n-1;
-    int mid = s+(e-s)/2;
-    int ans = -1;
-    while(s<=e){
+int lastOcc(int arr[], int n, int key) {
+  int s = 0;
+  int e = n - 1;
+  int mid = s + (e - s) / 2;
+  int ans = -1;
+  while (s <= e) {
 
-        if(arr[mid]==key){
-            ans = mid;
-            s = mid + 1; 
-        }
-        else if(key>arr[mid]){
-            s = mid + 1;
-        }
-        else if(key<arr[mid]){
-            e = mid - 1;
-        }
-        mid = s+(e-s)/2;
+    if (arr[mid] == key) {
+      ans = mid;
+      s = mid + 1;
+    } else if (key > arr[mid]) {
+      s = mid + 1;
+    } else if (key < arr[mid]) {
+      e = mid - 1;
     }
-    return ans;
+    mid = s + (e - s) / 2;
+  }
+  return ans;
 }
 
-int main(){
+int main() {
 
-    int arr[7] = {1,2,3,3,3,3,3};
-    int totalOcc = (lastOcc(arr,7,3)- firstOcc(arr,7,3))+1;
-    cout<<"Total time 3 occur in arr: "<<totalOcc<<endl;
-
+  int arr[7] = {1, 2, 3, 3, 3, 3, 3};
+  int totalOcc = (lastOcc(arr, 7, 3) - firstOcc(arr, 7, 3)) + 1;
+  cout << "Total time 3 occur in arr: " << totalOcc << endl;
 }
