6871263c60431819f1e82daac74f896dd672f9a3
TusharPatwal <patwaltushar001@gmail.com>
2023-01-03 09:16:45 +0530

    Write a program to implement the concept of Single Inheritance

---
+++ labWork/labfile/page7_Q1.cpp
@@ -0,0 +1,21 @@
+// Write a program to implement the concept of Single Inheritance?
+
+#include <iostream>
+using namespace std;
+
+class base {
+public:
+  int n, m;
+  void sum(int n, int m) { 
+    cout << n << " + " << m << " = " << n + m << endl; 
+    }
+};
+
+class derived : public base {
+public:
+};
+
+int main() {
+  derived obj;
+  obj.sum(2, 4);
+}
