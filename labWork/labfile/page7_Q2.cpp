09e89f19c27158e945921e6b246f242ce817dcd9
TusharPatwal <patwaltushar001@gmail.com>
2023-01-03 09:27:50 +0530

    multilevel inheritance in c++

---
+++ labWork/labfile/page7_Q2.cpp
@@ -0,0 +1,8 @@
+// Identify the type of inheritance and implement it by writing  program (assuming data members and member functions)
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+}
