29f98bf40678f7e0e0f57fc7c09949016e73ffe8
TusharPatwal <patwaltushar001@gmail.com>
2023-01-03 09:00:43 +0530

    Write a Program to check whether a number is prime or not, using classes

--- labWork/labfile/page3_Q2.cpp
+++ labWork/labfile/page3_Q2.cpp
@@ -7,30 +7,27 @@ class isPrime {
 public:
   int n;
   int prime(int n) {
-    if(n == 0 || n == 1){
-        return 0;
-    }
-    else{
-        for(int i=2;i<n;i++){
-            if(n%i==0){
-                return 0;
-            }
+    if (n == 0 || n == 1) {
+      return 0;
+    } else {
+      for (int i = 2; i < n; i++) {
+        if (n % i == 0) {
+          return 0;
         }
-        return 1;
-    } 
+      }
+      return 1;
+    }
   }
 };
 
 int main() {
-    isPrime obj;
-    int n;
-    cin>>n;
-    int m = obj.prime(n);
-    if(m==1){
-        cout<<n<<" is a prime number"<<endl;
-    }
-    else{
-        cout<<n<<" is not a prime number"<<endl;
-    }
-
+  isPrime obj;
+  int n;
+  cin >> n;
+  int m = obj.prime(n);
+  if (m == 1) {
+    cout << n << " is a prime number" << endl;
+  } else {
+    cout << n << " is not a prime number" << endl;
+  }
 }
