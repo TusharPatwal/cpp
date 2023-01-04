ef39443e6a5ce8fe36d980a3bddab051975fcfc6
TusharPatwal <patwaltushar001@gmail.com>
2023-01-04 20:02:34 +0530

    Make an inline function to find the square of a number

---
+++ labWork/labfile/page3_Q1.cpp
@@ -0,0 +1,17 @@
+// Make an inline function to find the square of a number
+#include <iostream>
+using namespace std;
+
+inline int square(int x) {
+  return x * x;
+}
+
+int main() {
+  int num;
+  cout << "Enter a number: ";
+  cin >> num;
+
+  // Call the inline function
+  int result = square(num);
+  cout << "The square of " << num << " is: " << result << endl;
+}
