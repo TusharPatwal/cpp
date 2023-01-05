f7fb18b2c9dae83d7d3152a09c3d8e6f7ee9f88f
TusharPatwal <patwaltushar001@gmail.com>
2023-01-05 17:40:02 +0530

    Write a Program to overload Unary Operators

---
+++ labWork/labfile/page13_Q1.cpp
@@ -0,0 +1,39 @@
+// Write a Program to overload Unary Operators
+
+#include <iostream>
+using namespace std;
+
+class Counter {
+public:
+  Counter(int start) : count(start) {}
+
+  // Overloaded ++ operator
+  Counter operator++() { 
+    return Counter(++count); }
+
+  // Overloaded -- operator
+  Counter operator--() { 
+    return Counter(--count); }
+
+  // Member function to return the current count
+  int GetCount() const { 
+    return count; }
+
+private:
+  int count;
+};
+
+int main() {
+  Counter c(91);
+  cout << "Count: " << c.GetCount() << endl;
+
+  // Increment the counter
+  ++c;
+  cout << "Count: " << c.GetCount() << endl;
+
+  // Decrement the counter
+  --c;
+  cout << "Count: " << c.GetCount() << endl;
+
+  return 0;
+}
