a6520dd4596cdb3263b4b6e9636493094b9f676b
TusharPatwal <patwaltushar001@gmail.com>
2023-01-07 20:50:18 +0530

    Search in Rotated Sorter array

---
+++ DSA/day011_binarySearch02.cpp
@@ -0,0 +1,54 @@
+// Search in Rotated Sorter array
+
+#include <iostream>
+using namespace std;
+
+int getPivot(int arr[], int n) {
+  int s = 0;
+  int e = n - 1;
+  int mid = s + (e - s) / 2;
+
+  while (s < e) {
+    if (arr[mid] >= arr[0]) {
+      s = mid + 1;
+    } else {
+      e = mid;
+    }
+    mid = s + (e - s) / 2;
+  }
+  return e;
+}
+int binarySearch(int arr[], int s, int e, int key) {
+  int start = s;
+  int end = e;
+  int mid = start + (end - start) / 2;
+
+  while (start <= end) {
+
+    if (arr[mid] == key) {
+      return mid;
+    }
+
+    if (key > arr[mid]) {
+      start = mid + 1;
+    } else {
+      end = mid - 1;
+    }
+    mid = start + (end - start) / 2;
+  }
+  return -1;
+}
+int firstPosition(int arr[], int n, int k) {
+  int pivot = getPivot(arr, n);
+
+  if (k >= arr[pivot] && k <= arr[n - 1]) {
+    return binarySearch(arr, pivot, n - 1, k);
+  } else {
+    return binarySearch(arr, 0, pivot - 1, k);
+  }
+}
+
+int main() {
+  int arr[6] = {5, 7, 9, 12, 1, 2};
+  cout << "Index is " << firstPosition(arr, 6, 1);
+}
