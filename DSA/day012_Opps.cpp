0d34e0fd95de5d3fb57344520420fa2565bfec59
TusharPatwal <patwaltushar001@gmail.com>
2023-01-12 20:27:10 +0530

    class student

--- DSA/day012_Opps.cpp
+++ DSA/day012_Opps.cpp
@@ -8,8 +8,27 @@ class print{
     }
 };
 
+class student{
+    public:
+    int roll_no;
+    string name[20];
+
+    void getdata(int roll_no, string name){
+        this-> roll_no = roll_no;
+        name = name;
+    }
+    void outdata(){
+        cout<<"Roll no = "<<roll_no<<endl;
+        cout<<"Name = "<<name<<endl;
+    }
+};
+
 int main(){
     print obj;
 
     obj.printf();
+
+    student obj1;
+    obj1.getdata(98,"tushar");
+    obj1.outdata();
 }
