16d12cf2afd8762bb6671ea655f9f4a375155087
TusharPatwal <patwaltushar001@gmail.com>
2023-01-12 20:44:02 +0530

    creating a class to find area of rectangle

--- DSA/day012_Opps.cpp
+++ DSA/day012_Opps.cpp
@@ -1,34 +1,51 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class print{
-    public:
-    void printf(){
-        cout<<"First class program."<<endl;
-    }
+class print {
+public:
+  void printf() { cout << "First class program." << endl; }
 };
 
-class student{
-    public:
-    int roll_no;
-    string name[20];
+class student {
+public:
+  int roll_no;
+  string name[20];
 
-    void getdata(int roll_no, string name){
-        this-> roll_no = roll_no;
-        name = name;
-    }
-    void outdata(){
-        cout<<"Roll no = "<<roll_no<<endl;
-        cout<<"Name = "<<name<<endl;
-    }
+  void getdata(int roll_no, string name) {
+    this->roll_no = roll_no;
+    name = name;
+  }
+  void outdata() {
+    cout << "Roll no = " << roll_no << endl;
+    cout << "Name = " << name << endl;
+  }
 };
 
-int main(){
-    print obj;
+class rect {
+public:
+  int length, breadth;
 
-    obj.printf();
+  void setdata(int i, int j) {
+    length = i;
+    breadth = j;
+  }
+  int area() { return (length * breadth); }
+};
 
-    student obj1;
-    obj1.getdata(98,"tushar");
-    obj1.outdata();
+int main() {
+  print obj;
+
+  obj.printf();
+
+  student obj1;
+  obj1.getdata(98, "tushar");
+  obj1.outdata();
+
+  rect obj2;
+  int x, y;
+  cout<<"Enter the value of x and y: "<<endl;  
+  cin >> x >> y;
+  obj2.setdata(x, y);
+
+  cout<<"Area of rectangle is: "<<obj2.area()<<endl;
 }
