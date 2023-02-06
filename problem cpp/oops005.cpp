dda3a1b1beda82bda1550a507617e03e114e7276
TusharPatwal <patwaltushar001@gmail.com>
2023-02-06 21:36:36 +0530

    static data member and function

--- problem cpp/oops005.cpp
+++ problem cpp/oops005.cpp
@@ -5,20 +5,37 @@ using namespace std;
 class employee{
     
     int id;
-    int count;
+    static int count;
     
     public:
     void setdata(void){
         cout<<"Enter the employee ID: ";
         cin>>id;
+        count++;
     }
     void getdata(void){
-        cout<<id;
+        cout<<"The id of employee is "<<id<<" and count is "<<count<<endl;
+    }
+
+    static void getCount(void){
+        cout<<"The value of count: "<<count<<endl;
     }
 };
+
+// count is the static data member of class employee
+int employee :: count=0; // default value of static data member is 0. you can pass value of static data member outside the class
 
 int main(){
     employee tushar;
     tushar.setdata();
     tushar.getdata();
+    employee::getCount();
+    
+    tushar.setdata();
+    tushar.getdata();
+    employee::getCount();
+
+    tushar.setdata();
+    tushar.getdata();
+    employee::getCount();
 }
